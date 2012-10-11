243f0c3aa2c480c6b600d3e13749d078eb270188
Kevin J. Woolley <kjw@pathological.ca>
2012-10-10 23:48:04 -0700

    Make it even more concise with make_pair().

--- unordered_map1.cc
+++ unordered_map1.cc
@@ -9,8 +9,8 @@ main(int argc, char **argv)
     using namespace std;
 
     unordered_map<string,string> to_blame;
-    to_blame.insert(pair<string,string>("Stroustrup", "C++"));
-    to_blame.insert(pair<string,string>("Wirth", "Modula"));
+    to_blame.insert(make_pair("Stroustrup", "C++"));
+    to_blame.insert(make_pair("Wirth", "Modula"));
 
     for (auto &ents: to_blame) {
         cout << ents.first << ": " << ents.second << endl;
